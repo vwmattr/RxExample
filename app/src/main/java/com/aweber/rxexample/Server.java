b001c1199849e54b7f077dc557c8ea9dbd8b1055
Matt R <mattr@aweber.com>
2014-11-06 13:45:24 -0500

    Add basic Retrofit code to make a simple API call to StackOverflow API

---
+++ app/src/main/java/com/aweber/rxexample/Server.java
@@ -0,0 +1,19 @@
+package com.aweber.rxexample;
+
+import retrofit.Callback;
+import retrofit.client.Response;
+import retrofit.http.GET;
+
+/**
+ * API service interface. Used by Retrofit to handle server
+ * communication.
+ */
+public interface Server {
+
+    static final String QUESTIONS_ENDPOINT =
+            "/2.2/questions?order=desc&sort=activity&tagged=android&site=stackoverflow";
+
+    @GET(QUESTIONS_ENDPOINT)
+    void questions(Callback<Response> callback);
+
+}
