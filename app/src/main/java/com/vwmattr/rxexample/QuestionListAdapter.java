097f55e7154a65302a2f1eac5d52f12e15c2a4bc
Reinman <matt.rein@mlb.com>
2015-11-16 15:22:07 -0500

    Update MainActivity and QuestionListAdapter to display result list of questions in a RecyclerView instead of a ListView.

--- app/src/main/java/com/vwmattr/rxexample/QuestionListAdapter.java
+++ app/src/main/java/com/vwmattr/rxexample/QuestionListAdapter.java
@@ -1,67 +1,56 @@
 package com.vwmattr.rxexample;
 
-import android.app.Activity;
-import android.text.Html;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
 import com.vwmattr.rxexample.entities.Question;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * List Adapter to display {@link com.vwmattr.rxexample.entities.QuestionList}.
  */
-public class QuestionListAdapter extends ArrayAdapter<Question> {
+public class QuestionListAdapter extends RecyclerView.Adapter<QuestionListAdapter.ViewHolder> {
 
-    private final Activity context;
     private List<Question> questions;
 
-    static class ViewHolder {
-        public TextView titleText;
-    }
-
-    public QuestionListAdapter(Activity context) {
-        super(context, R.layout.list_item_question, new ArrayList<Question>());
-        this.context = context;
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_question, parent, false);
+        return new ViewHolder(v);
     }
 
     @Override
-    public int getCount() {
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        final Question question = questions.get(position);
+
+        holder.titleText.setText(question.getTitle());
+        holder.itemView.setTag(question);
+    }
+
+    @Override
+    public int getItemCount() {
         if (questions == null) {
             return 0;
         }
         return questions.size();
     }
 
+    protected static class ViewHolder extends RecyclerView.ViewHolder {
+        public TextView titleText;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            titleText = (TextView) itemView.findViewById(R.id.title);
+        }
+    }
+
     public void setQuestions(List<Question> questions) {
         this.questions = questions;
         notifyDataSetChanged();
     }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View rowView = convertView;
-
-        if (rowView == null) {
-            LayoutInflater inflater = context.getLayoutInflater();
-            rowView = inflater.inflate(R.layout.list_item_question, null);
-            ViewHolder viewholder = new ViewHolder();
-            viewholder.titleText = (TextView) rowView.findViewById(R.id.title);
-            rowView.setTag(viewholder);
-        }
-
-        ViewHolder holder = (ViewHolder) rowView.getTag();
-        Question question = questions.get(position);
-
-        //fill 'er up
-        holder.titleText.setText(Html.fromHtml(question.getTitle()));
-
-        return rowView;
-    }
-
 }
